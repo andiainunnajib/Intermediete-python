b929456edfaff539cce2d5fa8f7cda3adf557231
andiainunnajib <andiainunajib59@gmail.com>
2021-06-19 07:06:29 +0700

    da

---
+++ Numpy/using_array_generating_function.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+#Create range
+x = np.arange(10)
+print(x)
+
+
+
+
+for i in range (10):
+    print(i)
+
+#create range
+x = np.arange(10, 20) #argument : start, stops
+print(x)
+
+
